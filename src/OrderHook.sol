d3d61171ad30136eadb6395c0bbcee4b5e017546
Atiq Ishrak <133586498+ExtraCaterpiller@users.noreply.github.com>
2024-10-15 01:54:01 +0600

    Update OrderHook.sol

--- src/OrderHook.sol
+++ src/OrderHook.sol
@@ -1,4 +1,4 @@
-// SPDX-License-Identifier: SEE LICENSE IN LICENSE
+// SPDX-License-Identifier: MIT
 pragma solidity 0.8.24;
 
 import {IVault} from "@balancer-labs/contracts/interfaces/contracts/vault/IVault.sol";
